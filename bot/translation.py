df94b97eee2473b24df17723c4a20df4937124c0
joker00745 <85333471+joker00745@users.noreply.github.com>
2021-06-08 12:15:53 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,7 +7,7 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Just you can't add me to your group 🚫
 
 For More Details Click Help Button Below..
 @CrazyBotsz
